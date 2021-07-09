210439a31259a82462711d9dc6a2af74890f69a8
CoronaMinsel <87165500+CoronaMinsel@users.noreply.github.com>
2021-07-09 14:02:02 +0800

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -2,4 +2,4 @@
 var googleDocURL = 'https://docs.google.com/spreadsheets/d/1O2QbVmQQc91WJ-BTFlrFfP3PNNZuYydoRjPAb_B3DCs/edit#gid=0';
 
 // insert your own Google Sheets API key from https://console.developers.google.com
-var googleApiKey = 'AIzaSyBh9nKnVZm2RPeZa0ywCOxPAgJJfK87WhY';
+var googleApiKey = 'AIzaSyBXw2_bPm7go1WL5IzgCpIpuYTLdWheAIk';
